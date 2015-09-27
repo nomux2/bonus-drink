8dfe6983eea0e4c593bae0b6c17a5dc9dc5e7915
nomux2 <misoyasu@cg8.so-net.ne.jp>
2015-09-28 06:51:11 +0900

    BonusDrink class develop

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,37 @@
 class BonusDrink
+
+  BONUS_DRINK_GET_COUNT = 3
+  @error = ""
+  
+	attr_accessor :error
+  
   def self.total_count_for(amount)
-    0
+    
+    #整数値チェック
+    if !amount.is_a?(Integer)
+      puts "error:#{amount} is not integer"
+      return -1
+    end
+    
+    #マイナスチェック
+    if amount < 0
+      puts "error:#{amount} is not positive number"
+      return -1
+    end
+    
+    #おまけがもらえる本数以下ならそのまま返す
+    if amount < BONUS_DRINK_GET_COUNT
+      return amount
+    end
+
+    # 3本で1本もらえるので、2本目以降のプレゼントは2本買えば1本もらえる
+    # したがって3本以上のときに最初にもらえるプレゼント(1本目)は別として、2本目以降は(3 - 1)本で1本もらえることになる
+    # (最初に購入した数 - 3) から (3-1)本を割った数が2本目以降にもらえる本数、それに1本目(最初にもらったボーナス分)を足す。
+    bonus = (amount - BONUS_DRINK_GET_COUNT) / (BONUS_DRINK_GET_COUNT - 1) + 1
+    
+    #もらえる本数を返す
+    return amount + bonus
+    
   end
+  
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,40 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
+
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
+  specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+  
+  specify "本数確認" do
+  
+    total_amount = 0
+    bonus_count = 0
+
+    #100回ループ
+    100.times do |i|
+      
+      bonus_count += 1
+      total_amount += 1
+      
+      #３本で1本交換
+      if bonus_count >= 3
+        bonus_count -= 3    #3本と引き換えに
+        bonus_count += 1    #1本もらう
+        total_amount += 1   #合計本数に追加
+      end
+      
+      puts i + 1
+      
+      expect(BonusDrink.total_count_for(i + 1)).to eq total_amount
+      
+    end
+  end
+  
+  #数値以外
+  specify { expect(BonusDrink.total_count_for("abc")).to eq -1 }
+  specify { expect(BonusDrink.total_count_for(1.25)).to eq -1 }
+  specify { expect(BonusDrink.total_count_for(-10)).to eq -1 }
+
 end
